1085ec9b2841e155c0b4ebd8820ad20e32ed2476
Gamy Malasarte <gmaasarte@Gamys-Mac.local>
2017-06-19 21:02:36 -0700

    Added assets

--- SidebarMenu/ShifterDetailVC.swift
+++ SidebarMenu/ShifterDetailVC.swift
@@ -87,20 +87,6 @@ class PokemonDetailVC: UIViewController {
         emerNameLabel.isHidden  = true
         emerNumLabel.isHidden   = true
         emailLabel.isHidden     = true
-    
-        // local pdf
-        //if let pdfURL = Bundle.main.url(forResource: "Alice_In_Wonderland", withExtension: "pdf", subdirectory: nil, localization: nil)  {
-        //    do {
-        //        let data = try Data(contentsOf: pdfURL)
-        //        let webView = WKWebView(frame: CGRect(x:20,y:120,width:view.frame.size.width-40, height:view.frame.size.height-40))
-        //        webView.load(data, mimeType: "application/pdf", characterEncodingName:"", baseURL: pdfURL.deletingLastPathComponent())
-        //        view.addSubview(webView)
-        //    }
-        //    catch {
-        //        // catch errors here
-        //    }
-        
-        //}
 
         // replace spaces with %20
         let waiverFile = "\(shifter.waiver_form)".replacingOccurrences(of: " ", with: "%20")
