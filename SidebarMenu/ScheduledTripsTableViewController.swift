996f213b7856659c6c1b4af1cd75ef7f61fd8d37
Gamy Malasarte <gmalasarte@Gamys-Mac.local>
2017-06-28 21:28:23 -0700

    asDasdasD

--- SidebarMenu/ScheduledTripsTableViewController.swift
+++ SidebarMenu/ScheduledTripsTableViewController.swift
@@ -35,22 +35,43 @@ class ScheduledTripsTableViewController: UITableViewController {
         myTableView.dataSource = self
         
         // Set the firebase reference
-        //ref = Database.database().reference()
+        ref = Database.database().reference()
         // Retrieve the posts and listen for changes
         
-        //databaseHandle = ref?.child("scheduledtrips/gamy316").observe(.childAdded, with: { (snapshot) in
+        //databaseHandle = ref?.child("scheduledtrips/gamy316/06282017 05:48 PM/").observe(.childAdded, with: { (snapshot) in
         
-        Database.database().reference().child("scheduledtrips/gamy316").observe(.childAdded, with: { (snapshot) in
+        Database.database().reference().child("scheduledtrips/gamy316/").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let result = snapshot.children.allObjects as? [DataSnapshot] {
+                //print(result)
+                for snap in result {
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        //print(postDict["patientid"]! as! String)
+                        //print(postDict["pickupdatetime"]! as! String)
+                        //print(postDict["pickupfrom"]! as! String)
+                        //print(postDict["pickupto"]! as! String)
+                        
+                        self.postData.append(postDict["patientid"]! as! String)
+                        self.postData.append(postDict["pickupdatetime"]! as! String)
+                        self.postData.append(postDict["pickupfrom"]! as! String)
+                        self.postData.append(postDict["pickupto"]! as! String)
+                    }
+                }
+            }
+            
+            
+            
             
             // Try to convert the value of the data to a string
-            let post = snapshot.value as? String
+            //let post = snapshot.value as? String
             
-            if let actualPost = post {
+            //if let actualPost = post {
                 // Append the data to our postData array
-                self.postData.append(actualPost)
+            //    self.postData.append(actualPost)
                 // Reload the tableview
-                self.myTableView.reloadData()
-            }
+            //    self.myTableView.reloadData()
+            //}
+            
+            self.myTableView.reloadData()
         })
  
     }
