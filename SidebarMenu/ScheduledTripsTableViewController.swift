8d8111c2be59844f76ed71951738459f6d51b3ae
Gamy Malasarte <gmalasarte@Gamys-Mac.local>
2017-06-28 20:03:12 -0700

    asdfasdf

--- SidebarMenu/ScheduledTripsTableViewController.swift
+++ SidebarMenu/ScheduledTripsTableViewController.swift
@@ -1,65 +1,74 @@
-//
-//  ScheduledTripsTableViewController.swift
-//  SidebarMenu
-//
 
 import UIKit
+import FirebaseDatabase
 
 class ScheduledTripsTableViewController: UITableViewController {
-
+    
     @IBOutlet var menuButton:UIBarButtonItem!
     @IBOutlet var extraButton:UIBarButtonItem!
     
+
+    @IBOutlet weak var myTableView: UITableView!
+ 
+    
+    var ref:DatabaseReference?
+    var databaseHandle:DatabaseHandle?
+    
+    var postData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if revealViewController() != nil {
             menuButton.target = revealViewController()
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
-
-            revealViewController().rightViewRevealWidth = 150
+            
+            revealViewController().rightViewRevealWidth = 200
             extraButton.target = revealViewController()
             extraButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
-
+            
             view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
             
-        
+            
         }
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
+        // Set the firebase reference
+        ref = Database.database().reference()
+        // Retrieve the posts and listen for changes
+        databaseHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
+            
+            // Try to convert the value of the data to a string
+            let post = snapshot.value as? String
+            if let actualPost = post {
+                // Append the data to our postData array
+                self.postData.append(actualPost)
+                // Reload the tableview
+                self.myTableView.reloadData()
+            }
+        })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // Return the number of sections.
-        return 1
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Return the number of rows in the section.
-        return 1
+        return postData.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ScheduledTripsTableViewCell
-
-        // Configure the cell...
-        if indexPath.row == 0 {
-            cell.fromLabel.text = "668 Holland Heights Ave. Las Vegas, NV 89123"
-            cell.toLabel.text = "7266 Summer Grove Ave Las Vegas, NV 89000"
-            cell.whenLabel.text = "June 23, 2017 12:00pm"
-        }
-        
-        
-
-        return cell
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "PostCell")
+        cell?.textLabel?.text = postData[indexPath.row]
+        return cell!
     }
     
-
 }
+
+
