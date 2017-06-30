11bede8ac959e90f56fd87ca0752b9ac66cc7ce7
Gamy Malasarte <gmalasarte@Gamys-Mac.local>
2017-06-30 16:21:22 -0700

    delete cells

--- SidebarMenu/ScheduledTripsTableViewController.swift
+++ SidebarMenu/ScheduledTripsTableViewController.swift
@@ -1,17 +1,17 @@
 import UIKit
 import FirebaseDatabase
 
-class ScheduledTripsTableViewController: UITableViewController /*, UITableViewDataSource, UITableViewDelegate*/{
+
+class ScheduledTripsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var menuButton:UIBarButtonItem!
     @IBOutlet var extraButton:UIBarButtonItem!
     
-    var deleteCellIndexPath: NSIndexPath? = nil
     
+    @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var myTableView: UITableView!
- 
-    //@IBOutlet weak var fromLabel: UILabel!
+    //var PostDatas = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Team Pluto!"]
+    var deletePostDataIndexPath: IndexPath? = nil
     
     var ref:DatabaseReference?
     var databaseHandle:DatabaseHandle?
@@ -20,14 +20,12 @@ class ScheduledTripsTableViewController: UITableViewController /*, UITableViewDa
     
     var postData = [String]()
     
-    override func viewDidAppear(_ animated: Bool) {
-        //
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
         // firebase database init
+        ref = Database.database().reference()
         
         // preferences init
         let preferences = UserDefaults.standard
@@ -35,33 +33,33 @@ class ScheduledTripsTableViewController: UITableViewController /*, UITableViewDa
         
         self.title = "Scheduled trips for: " + patientId
         
+        
         if revealViewController() != nil {
             menuButton.target = revealViewController()
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             
-         
+            
             revealViewController().rightViewRevealWidth = 200
             extraButton.target = revealViewController()
             extraButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
             
-            //view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        
+            // Disable gesture recognizer so swiping left can be enabled
+            // view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
         }
- 
-
         // Do any additional setup after loading the view, typically from a nib.
-        myTableView.delegate = self
-        myTableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // Retrieve the posts and listen for changes
         Database.database().reference().child("scheduledtrips/" + patientId).observe(.childAdded, with: { (snapshot) in
-    
-           if let result = snapshot.children.allObjects as? [DataSnapshot] {
+            
+            if let result = snapshot.children.allObjects as? [DataSnapshot] {
                 //print("\(result)")
                 var fromString:String = ""
                 var toString:String = ""
                 var whenString:String = ""
-            
+                
                 for snap in result {
                     if (snap.key != "dateadded") {
                         if (snap.key == "pickup") {
@@ -73,84 +71,76 @@ class ScheduledTripsTableViewController: UITableViewController /*, UITableViewDa
                         if (snap.key == "pickupdate") {
                             whenString = snap.value as! String
                         }
-
+                        
                     }
                 }
-           
+                
                 self.postData.append("From: \(fromString)\nTo: \(toString)\nWhen: \(whenString)\n")
-            
-           } else {
+                
+            } else {
                 print("Error retrieving FrB data") // snapshot value is nil
             }
             
-            self.myTableView.reloadData()
+            self.tableView.reloadData()
         })
- 
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //MARK: UITableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return postData.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = myTableView.dequeueReusableCell(withIdentifier: "PostCell")
-        //cell?.textLabel?.text = postData[indexPath.row]
-        cell?.textLabel?.font =  UIFont.systemFont(ofSize: 14.0)
-        cell?.textLabel?.numberOfLines = 4
-        cell?.textLabel?.text = postData[indexPath.row]
-        return cell!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")!
+        cell.textLabel?.text = postData[indexPath.row]
+        cell.textLabel?.font =  UIFont.systemFont(ofSize: 14.0)
+        cell.textLabel?.numberOfLines = 4
+        return cell
     }
     
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    // MARK: UITableViewDelegate Methods
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            deleteCellIndexPath = indexPath
-            let cellToDelete = postData[indexPath.row]
-            confirmDelete(cell: cellToDelete)
+            deletePostDataIndexPath = indexPath
+            let PostDataToDelete = postData[indexPath.row]
+            confirmDelete(PostDataToDelete)
         }
     }
-
+    
+    // Delete Confirmation and Handling
+    func confirmDelete(_ PostData: String) {
+        let alert = UIAlertController(title: "Delete PostData", message: "Are you sure you want to permanently delete \(postData)?", preferredStyle: .actionSheet)
         
-    func confirmDelete(cell: String) {
-        let alert = UIAlertController(title: "Delete Planet", message: "Are you sure you want to permanently delete \(postData)?", preferredStyle: .actionSheet)
-        
-        let DeleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: handleDeletePlanet)
-        let CancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: cancelDeletePlanet)
+        let DeleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: handleDeletePostData)
+        let CancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: cancelDeletePostData)
         
         alert.addAction(DeleteAction)
         alert.addAction(CancelAction)
         
-        // Support display in iPad
+        // Support presentation in iPad
         alert.popoverPresentationController?.sourceView = self.view
         alert.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.size.width / 2.0, y: self.view.bounds.size.height / 2.0, width: 1.0, height: 1.0)
         
         self.present(alert, animated: true, completion: nil)
     }
     
-    
-    func handleDeletePlanet(_ alertAction: UIAlertAction!) -> Void {
-        if let indexPath = deleteCellIndexPath {
+    func handleDeletePostData(_ alertAction: UIAlertAction!) -> Void {
+        if let indexPath = deletePostDataIndexPath {
             tableView.beginUpdates()
             
             postData.remove(at: indexPath.row)
             
             // Note that indexPath is wrapped in an array:  [indexPath]
-            tableView.deleteRows(at: [indexPath as IndexPath], with: .automatic)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
             
-            deleteCellIndexPath = nil
+            deletePostDataIndexPath = nil
             
             tableView.endUpdates()
         }
     }
     
-    func cancelDeletePlanet(_ alertAction: UIAlertAction!) {
-        deleteCellIndexPath = nil
+    func cancelDeletePostData(_ alertAction: UIAlertAction!) {
+        deletePostDataIndexPath = nil
     }
 }
-
 
