e619c67eb17e227969fe3fded3a95770db0418e8
Gamy Malasarte <gmalasarte@Gamys-Mac.local>
2017-07-20 18:24:05 -0700

    with SMS messaging

--- SidebarMenu/ResetPasswordViewController.swift
+++ SidebarMenu/ResetPasswordViewController.swift
@@ -26,17 +26,7 @@ class ResetPasswordViewController: UIViewController {
 
     
     @IBAction func resetPasswordButtonTapped(_ sender: Any) {
-        PhoneAuthProvider.provider().verifyPhoneNumber("+17022736420") { (verificationID, error) in
-            if ((error) != nil) {
-                // Verification code not sent.
-                print(error as Any)
-            } else {
-                // Successful. -> it's sucessfull here
-                print(verificationID as Any)
-                UserDefaults.standard.set(verificationID, forKey: "firebase_verification")
-                UserDefaults.standard.synchronize()
-            }
-        }
+        reesetPassword(phoneNumber: "7022736420", firstName:  "Gamy")
     }
 
 
@@ -52,6 +42,57 @@ class ResetPasswordViewController: UIViewController {
         super.touchesBegan(touches, with: event)
     }
 
+    
+    
+    func reesetPassword(phoneNumber:String, firstName:String) {
+        
+        let url:URL = URL(string: "https://textbelt.com/text")!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+        let paramString = "phone=\(phoneNumber)&message=\(firstName)&key=7aeb4e18f8328b578a5f9a2e7ca6fec8980edb53dzQLAbRJjzd2e7IbrOc1bH8vg"
+        
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
+                return
+            }
+            
+            let json: Any?
+            
+            do {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            } catch {
+                return
+            }
+            
+            guard let server_response = json as? NSDictionary else {
+                return
+            }
+            
+            //print(server_response)
+            
+            // If data_block is empty, session id would be missing
+            if let data_block = server_response["sending"] as? NSDictionary {
+                print(data_block)
+            } else {
+                //
+            }
+            
+        })
+        
+        task.resume()
+        
+        
+    }
+    
     
     
 }
